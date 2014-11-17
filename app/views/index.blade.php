0ccdd3e5648aa63c3412a9a0bf5b3892fc79e16d
Donald Zhan <donald.zhan1984@gmail.com>
2014-11-17 21:54:00 +0800

    initial the laravel env

---
+++ app/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('content')
+<h1>揪路霸</h1>
+<h3>揪出一群侵佔公有地，作為私人用途之地霸行徑！</h3>
+@stop
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="zh-hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>揪地霸</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
